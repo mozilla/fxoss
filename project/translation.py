93653232fef00650876c6d3698392ee7e75959b2
Mark Lavin <mlavin@caktusgroup.com>
2014-01-29 13:55:56 -0500

    Register link titles for translation as well.

--- project/translation.py
+++ project/translation.py
@@ -3,8 +3,8 @@ from copy import deepcopy
 
 from django.contrib import admin
 
-from mezzanine.pages.admin import PageAdmin
-from mezzanine.pages.models import Page, RichTextPage
+from mezzanine.pages.admin import PageAdmin, LinkAdmin
+from mezzanine.pages.models import Page, RichTextPage, Link
 from modeltranslation.admin import TranslationAdmin
 from modeltranslation.translator import translator, TranslationOptions
 
@@ -17,12 +17,17 @@ class RichTextPageTranslationOptions(PageTranslationOptions):
     fields = ('content', )
 
 
+class LinkTranslationOptions(TranslationOptions):
+    fields = ('title', )
+
+
 translator.register(Page, PageTranslationOptions)
 translator.register(RichTextPage, RichTextPageTranslationOptions)
+translator.register(Link, LinkTranslationOptions)
 
 
 class TranslatedPageAdmin(PageAdmin, TranslationAdmin):
-    pass
+    """Combined Page admin class for Mezzanine and modeltranslation."""
 
 
 rich_text_fieldsets = deepcopy(TranslatedPageAdmin.fieldsets)
@@ -32,7 +37,13 @@ class TranslatedRichTextPageAdmin(TranslatedPageAdmin):
     fieldsets = rich_text_fieldsets
 
 
+class TranslatedLinkAdmin(LinkAdmin, TranslationAdmin):
+    """Combined Link admin class for Mezzanine and modeltranslation."""
+
+
 admin.site.unregister(Page)
 admin.site.unregister(RichTextPage)
+admin.site.unregister(Link)
 admin.site.register(Page, TranslatedPageAdmin)
 admin.site.register(RichTextPage, TranslatedRichTextPageAdmin)
+admin.site.register(Link, TranslatedLinkAdmin)
