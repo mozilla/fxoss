e1aaa2bf2746e2346790498f3b01912e5792317f
Mark Lavin <mlavin@caktusgroup.com>
2014-01-29 16:49:34 -0500

    Register Form model for translation in the admin.

--- project/translation.py
+++ project/translation.py
@@ -3,10 +3,11 @@ from copy import deepcopy
 
 from django.contrib import admin
 
+from mezzanine.forms.admin import FormAdmin, FieldAdmin
 from mezzanine.forms.models import Form, Field
 from mezzanine.pages.admin import PageAdmin, LinkAdmin
 from mezzanine.pages.models import Page, RichTextPage, Link
-from modeltranslation.admin import TranslationAdmin
+from modeltranslation.admin import TranslationAdmin, TranslationTabularInline
 from modeltranslation.translator import translator, TranslationOptions
 
 
@@ -52,9 +53,24 @@ class TranslatedLinkAdmin(LinkAdmin, TranslationAdmin):
     """Combined Link admin class for Mezzanine and modeltranslation."""
 
 
+class TranslatedFieldAdmin(FieldAdmin, TranslationTabularInline):
+    """Inline admin for translated Fields."""
+
+    fields = ['label', 'field_type', 'required', 'visible',
+        'choices', 'default', 'help_text', '_order']
+
+
+class TranslatedFormAdmin(FormAdmin, TranslationAdmin):
+    """Combined Form admin class for Mezzanine and modeltranslation."""
+
+    inlines = (TranslatedFieldAdmin, )
+
+
 admin.site.unregister(Page)
 admin.site.unregister(RichTextPage)
 admin.site.unregister(Link)
+admin.site.unregister(Form)
 admin.site.register(Page, TranslatedPageAdmin)
 admin.site.register(RichTextPage, TranslatedRichTextPageAdmin)
 admin.site.register(Link, TranslatedLinkAdmin)
+admin.site.register(Form, TranslatedFormAdmin)
