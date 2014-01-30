c889c6c12c0ff681032cb4a257ffb5f00f07ff32
Mark Lavin <mlavin@caktusgroup.com>
2014-01-30 10:14:15 -0500

    Change default TranslationAdmin behavior to show at most 2 languages.

--- project/translation.py
+++ project/translation.py
@@ -2,13 +2,16 @@
 from copy import deepcopy
 
 from django.contrib import admin
+from django.utils.translation import get_language, get_language_from_request, override
 
 from mezzanine.forms.admin import FormAdmin, FieldAdmin
 from mezzanine.forms.models import Form, Field
 from mezzanine.pages.admin import PageAdmin, LinkAdmin
 from mezzanine.pages.models import Page, RichTextPage, Link
+from modeltranslation import settings
 from modeltranslation.admin import TranslationAdmin, TranslationTabularInline
 from modeltranslation.translator import translator, TranslationOptions
+from modeltranslation.utils import build_localized_fieldname, get_translation_fields
 
 
 class PageTranslationOptions(TranslationOptions):
@@ -38,7 +41,58 @@ translator.register(Form, FormTranslationOptions)
 translator.register(Field, FieldTranslationOptions)
 
 
-class TranslatedPageAdmin(PageAdmin, TranslationAdmin):
+def _get_translation_fields(field):
+    """Instead of adding fields for every available language only
+    fields for the default and selected language are added."""
+    languages = [settings.DEFAULT_LANGUAGE, ]
+    active = get_language().split('-')[0].lower()
+    if active not in languages and active in settings.AVAILABLE_LANGUAGES:
+        languages.append(active)
+    return [build_localized_fieldname(field, l) for l in languages]
+
+
+class SlimTranslationAdmin(TranslationAdmin):
+    """Subclass of TranslationAdmin to only show at most two languages: the base
+    translation language (EN) and the selected language for the user."""
+
+    def replace_orig_field(self, option):
+        """Override of the default replace_orig_field for django-modeltranslation.
+        This handles both the original replacement of the fields and the language
+        switching case.
+        """
+        if option:
+            option_clean = list(option)
+            # Make one pass to replace translated field names with original names
+            for opt in option:
+                for field in self.trans_opts.fields:
+                    tranlated = get_translation_fields(field)
+                    if opt in get_translation_fields(field):
+                        if field not in option_clean:
+                            index = option_clean.index(opt)
+                            option_clean[index:index + 1] = [field]
+                        else:
+                            option_clean.remove(opt)
+            option_new = list(option_clean)               
+            for opt in option_clean:
+                if opt in self.trans_opts.fields:
+                    index = option_new.index(opt)
+                    option_new[index:index + 1] = _get_translation_fields(opt)
+                elif isinstance(opt, (tuple, list)) and (
+                        [o for o in opt if o in self.trans_opts.fields]):
+                    index = option_new.index(opt)
+                    option_new[index:index + 1] = self.replace_orig_field(opt)
+            option = option_new
+        return option
+
+    def _do_get_form_or_formset(self, request, obj, **kwargs):
+        """Activate the current language from the request before patching the fields."""
+        language = get_language_from_request(request, check_path=True)
+        with override(language):
+            result = super(SlimTranslationAdmin, self)._do_get_form_or_formset(request, obj, **kwargs)
+        return result
+
+
+class TranslatedPageAdmin(PageAdmin, SlimTranslationAdmin):
     """Combined Page admin class for Mezzanine and modeltranslation."""
 
 
@@ -49,7 +103,7 @@ class TranslatedRichTextPageAdmin(TranslatedPageAdmin):
     fieldsets = rich_text_fieldsets
 
 
-class TranslatedLinkAdmin(LinkAdmin, TranslationAdmin):
+class TranslatedLinkAdmin(LinkAdmin, SlimTranslationAdmin):
     """Combined Link admin class for Mezzanine and modeltranslation."""
 
 
@@ -60,7 +114,7 @@ class TranslatedFieldAdmin(FieldAdmin, TranslationTabularInline):
         'choices', 'default', 'help_text', '_order']
 
 
-class TranslatedFormAdmin(FormAdmin, TranslationAdmin):
+class TranslatedFormAdmin(FormAdmin, SlimTranslationAdmin):
     """Combined Form admin class for Mezzanine and modeltranslation."""
 
     inlines = (TranslatedFieldAdmin, )
