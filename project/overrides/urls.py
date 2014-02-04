5a1c375ad29ed7e9deb9198de2a2595d87e71825
David Ray <davidray@gmail.com>
2014-02-04 07:47:04 -0500

    confirm to app namespace in #14; WIP registration

---
+++ project/overrides/urls.py
@@ -0,0 +1,18 @@
+from __future__ import unicode_literals
+
+from django.conf.urls import patterns, url
+
+from mezzanine.conf import settings
+
+
+ACCOUNT_URL = getattr(settings, "ACCOUNT_URL", "/account/")
+SIGNUP_URL = getattr(settings, "SIGNUP_URL",
+                     "/%s/signup/" % ACCOUNT_URL.strip("/"))
+
+_slash = "/" if settings.APPEND_SLASH else ""
+
+urlpatterns = patterns("project.overrides.views",
+    url("^%s%s$" % (SIGNUP_URL.strip("/"), _slash),
+        "signup", name="signup"),
+)
+
