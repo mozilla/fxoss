4f0521d84a84108d7d592d8db9b92920b2541e74
Mark Lavin <mlavin@caktusgroup.com>
2014-02-05 10:12:36 -0500

    Change the default link admin to allow having links which are only shown to authenticated users.

--- protected_assets/admin.py
+++ protected_assets/admin.py
@@ -1,3 +1,20 @@
+from copy import deepcopy
+
 from django.contrib import admin
 
-# Register your models here.
+from mezzanine.pages.admin import  LinkAdmin as BaseLinkAdmin
+from mezzanine.pages.models import Link
+
+
+link_fieldsets = deepcopy(BaseLinkAdmin.fieldsets)
+link_fieldsets[0][1]["fields"] += ("login_required", )
+
+
+class LinkAdmin(BaseLinkAdmin):
+    """Customization of LinkAdmin to allow making links only display to authenticated users."""
+
+    fieldsets = link_fieldsets
+
+
+admin.site.unregister(Link)
+admin.site.register(Link, LinkAdmin)
