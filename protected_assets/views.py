10cbfef196a87f79c0d7c918532847913a67bdd5
Mark Lavin <mlavin@caktusgroup.com>
2014-02-05 09:09:07 -0500

    Check that user has signed the current version of the agreement.

--- protected_assets/views.py
+++ protected_assets/views.py
@@ -2,20 +2,23 @@ from __future__ import unicode_literals
 
 import os
 
-from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.views.static import serve
 
 from filebrowser_safe.settings import DIRECTORY as FILEBROWSER_DIRECTORY
+from mezzanine.conf import settings
+
+from .models import Agreement
 
 
 @login_required
 def protected_download(request, path):
     """Check for a signed download agreement before delivering the asset."""
-    # TODO: Define what it means to have signed the agreement
-    if not request.user.is_staff:
+    settings.use_editable()
+    agreement = Agreement.objects.filter(user=request.user, version=settings.DOWNLOAD_AGREEMENT_VERSION)
+    if not agreement.exists():
         return redirect('page', kwargs={'slug': 'download-agreement'})
     if settings.DEBUG:
         response = serve(request, path, document_root=os.path.join(settings.MEDIA_ROOT, FILEBROWSER_DIRECTORY, 'protected'))
