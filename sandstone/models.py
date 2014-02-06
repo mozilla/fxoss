4a9c6b4f2bc3191c420dbd5ab2418db4bc209350
Mark Lavin <mlavin@caktusgroup.com>
2014-02-06 15:59:15 -0500

    Sketching out models for storing TinyMCE snippets.

--- sandstone/models.py
+++ sandstone/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+from mezzanine.core.fields import RichTextField
+
+
+class TinyMCESnippet(models.Model):
+    """Stores small snippets for common content patterns."""
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    content = RichTextField()
