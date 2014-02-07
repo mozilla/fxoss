4bc8e8dfd17493c2ce2f41f89a7031e6ff9215c7
Mark Lavin <mlavin@caktusgroup.com>
2014-02-07 11:00:11 -0500

    Register with the admin.

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TinyMCESnippet
+
+
+class TinyMCESnippetAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', )
+
+
+admin.site.register(TinyMCESnippet, TinyMCESnippetAdmin)
