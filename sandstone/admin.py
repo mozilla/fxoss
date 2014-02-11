2e9d96015ee2660b58072fdab0502e80019109a0
David Ray <davidray@gmail.com>
2014-02-11 11:45:06 -0500

    Work on #46

--- sandstone/admin.py
+++ sandstone/admin.py
@@ -7,10 +7,11 @@ from mezzanine.pages.models import RichTextPage
 
 
 rt_page_fieldsets = deepcopy(PageAdmin.fieldsets)
-rt_page_fieldsets[0][1]["fields"].insert(3, "subtitle")
-rt_page_fieldsets[0][1]["fields"].insert(4, "intro")
-rt_page_fieldsets[0][1]["fields"].insert(5, "content")
-rt_page_fieldsets[0][1]["fields"].insert(6, "closing")
+rt_page_fieldsets[0][1]["fields"].insert(3, "intro")
+rt_page_fieldsets[0][1]["fields"].insert(4, "inherit")
+rt_page_fieldsets[0][1]["fields"].insert(5, "cta_title")
+rt_page_fieldsets[0][1]["fields"].insert(6, "cta_body")
+rt_page_fieldsets[0][1]["fields"].insert(7, "content")
 
 
 class SandstoneRichTextPageAdmin(PageAdmin):
