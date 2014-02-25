1ba1b2a297c2c3dac4287607ba29501599b0693f
David Ray <davidray@gmail.com>
2014-02-25 15:38:47 -0500

    Add basic concurrency lock tests for RichTextPage and Form utlizing provided library's mixins

--- sandstone/tests.py
+++ sandstone/tests.py
@@ -1,3 +1,21 @@
+from concurrency.utils import ConcurrencyTestMixin
 from django.test import TestCase
 
-# Create your tests here.
+from mezzanine.forms.models import Form
+from mezzanine.pages.models import RichTextPage
+
+
+class RichTextPageLockTestCase(ConcurrencyTestMixin, TestCase):
+    concurrency_model = RichTextPage
+    concurrency_kwargs = {
+        'title': 'test',
+        'content': 'Foo',
+    }
+
+    def setUp(self):
+        self.obj = self.concurrency_model(**self.concurrency_kwargs)
+        self.obj.save()
+
+
+class FormLockTestCase(RichTextPageLockTestCase):
+    concurrency_model = Form
