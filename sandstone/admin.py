172cafdd7b5b4b8bb06043d77afb4892b64d43d8
David Ray <davidray@gmail.com>
2014-02-25 07:52:35 -0500

    WIP #109; add locking

--- sandstone/admin.py
+++ sandstone/admin.py
@@ -1,3 +1,7 @@
+from concurrency import forms
+from concurrency.admin import ConcurrencyActionMixin, ConcurrencyListEditableMixin
+from concurrency.forms import VersionWidget
+
 from copy import deepcopy
 
 from django.contrib import admin
@@ -12,10 +16,14 @@ rt_page_fieldsets[0][1]["fields"].insert(4, "inherit")
 rt_page_fieldsets[0][1]["fields"].insert(5, "cta_title")
 rt_page_fieldsets[0][1]["fields"].insert(6, "cta_body")
 rt_page_fieldsets[0][1]["fields"].insert(7, "content")
+rt_page_fieldsets[0][1]["fields"].insert(8, "version")
 
 
-class SandstoneRichTextPageAdmin(PageAdmin):
+class SandstoneRichTextPageAdmin(ConcurrencyActionMixin,
+                                 ConcurrencyListEditableMixin,
+                                 PageAdmin):
     fieldsets = rt_page_fieldsets
+    formfield_overrides = {forms.VersionField: {'widget': VersionWidget}}
 
 admin.site.unregister(RichTextPage)
 admin.site.register(RichTextPage, SandstoneRichTextPageAdmin)
