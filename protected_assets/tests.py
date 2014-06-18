6baf2c0d141c0b5f6fdf5188378db2fe8b903800
Josh Mize <jgmize@gmail.com>
2014-06-18 15:39:53 -0500

    Fix flake8 errors

--- protected_assets/tests.py
+++ protected_assets/tests.py
@@ -20,15 +20,16 @@ from .views import export_csv
 User = get_user_model()
 
 
-
 class AgreementMixin(object):
     """Base setup and helpers for testing user agreement related views."""
 
     def setUp(self):
         activate('en')
-        self.agreement = Agreement.objects.create(version='v1.0', agreement_pdf='blah.pdf')
+        self.agreement = Agreement.objects.create(
+            version='v1.0', agreement_pdf='blah.pdf')
         self.agreement_url = reverse('protected_assets.sign_agreement')
-        self.asset_url = reverse('protected_assets.views.protected_download', args=['test.png', ])
+        self.asset_url = reverse(
+            'protected_assets.views.protected_download', args=['test.png', ])
         self.user = self.create_user(username='test', password='test')
         self.client.login(username='test', password='test')
 
@@ -59,25 +60,36 @@ class ProtectAssetTestCase(AgreementMixin, TestCase):
     """Integration test for attempting to download a protected asset."""
 
     def test_agreement_redirect(self):
-        """If user has not signed the agreement they should be redirected to sign."""
+        """
+        If user has not signed the agreement they should be redirected to sign.
+        """
         response = self.client.get(self.asset_url)
-        expected_url = '%s?%s' % (self.agreement_url, urlencode({'next': self.asset_url}))
+        expected_url = '?'.join(
+            [self.agreement_url, urlencode({'next': self.asset_url})])
         self.assertRedirects(response, expected_url)
 
     def test_already_signed(self):
-        """Download should be trigged if the agreement was previously signed."""
+        """
+        Download should be trigged if the agreement was previously signed.
+        """
         self.create_signed_agreement(user=self.user)
         response = self.client.get(self.asset_url)
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response['Content-Disposition'], 'attachment; filename=test.png')
-        self.assertEqual(response['X-Accel-Redirect'], '/__protected__/test.png')
+        self.assertEqual(
+            response['Content-Disposition'], 'attachment; filename=test.png')
+        self.assertEqual(
+            response['X-Accel-Redirect'], '/__protected__/test.png')
 
     def test_signed_old_version(self):
-        """User will need to resign the agreement if they signed and older version."""
-        old_agreement = Agreement.objects.create(version='v0.9', agreement_pdf='blah.pdf')
+        """
+        User will need to resign the agreement if they signed an older version.
+        """
+        old_agreement = Agreement.objects.create(
+            version='v0.9', agreement_pdf='blah.pdf')
         self.create_signed_agreement(user=self.user, agreement=old_agreement)
         response = self.client.get(self.asset_url)
-        expected_url = '%s?%s' % (self.agreement_url, urlencode({'next': self.asset_url}))
+        expected_url = '?'.join(
+            [self.agreement_url, urlencode({'next': self.asset_url})])
         self.assertRedirects(response, expected_url)
 
     def test_return_to_referring_page(self):
@@ -85,7 +97,8 @@ class ProtectAssetTestCase(AgreementMixin, TestCase):
         response = self.client.get(self.asset_url, HTTP_REFERER='/')
         expected_url = '%s?%s' % (self.agreement_url, urlencode({'next': '/'}))
         self.assertRedirects(response, expected_url)
-        self.assertEqual(self.client.session['waiting_download'], self.asset_url)
+        self.assertEqual(
+            self.client.session['waiting_download'], self.asset_url)
 
 
 @override_settings(DOWNLOAD_AGREEMENT_VERSION='v1.0')
@@ -96,13 +109,15 @@ class SignAgreementTestCase(AgreementMixin, TestCase):
         """Create Agreement record when user signs the form."""
         response = self.client.post(self.agreement_url, data={'agree': 'on'})
         self.assertRedirects(response, '/')
-        self.assertTrue(SignedAgreement.objects.filter(user=self.user).exists())
+        self.assertTrue(
+            SignedAgreement.objects.filter(user=self.user).exists())
 
     def test_did_not_agree(self):
         """No record should be created if they didn't agree."""
         response = self.client.post(self.agreement_url, data={})
         self.assertEqual(response.status_code, 200)
-        self.assertFalse(SignedAgreement.objects.filter(user=self.user).exists())
+        self.assertFalse(
+            SignedAgreement.objects.filter(user=self.user).exists())
 
     def test_next_redirect(self):
         """Redirect user to custom location."""
@@ -110,11 +125,14 @@ class SignAgreementTestCase(AgreementMixin, TestCase):
         url = '%s?next=%s' % (self.agreement_url, self.asset_url)
         response = self.client.post(url, data={'agree': 'on'})
         self.assertRedirects(response, next_url)
-        self.assertTrue(SignedAgreement.objects.filter(user=self.user).exists())
+        self.assertTrue(
+            SignedAgreement.objects.filter(user=self.user).exists())
 
     def test_mark_download_as_ready(self):
-        """If download is waiting in the session it should be marked as ready."""
-        session  = self.client.session
+        """
+        If download is waiting in the session it should be marked as ready.
+        """
+        session = self.client.session
         session['waiting_download'] = self.asset_url
         session.save()
         response = self.client.post(self.agreement_url, data={'agree': 'on'})
@@ -130,13 +148,17 @@ class SignAgreementTestCase(AgreementMixin, TestCase):
 
     def test_process_request_with_proxy(self):
         """Check IP of agreement with a single proxy in place."""
-        self.client.post(self.agreement_url, data={'agree': 'on'}, HTTP_X_FORWARDED_FOR='1.1.1.1')
+        self.client.post(
+            self.agreement_url, data={'agree': 'on'},
+            HTTP_X_FORWARDED_FOR='1.1.1.1')
         agreement = SignedAgreement.objects.get(user=self.user)
         self.assertEqual('1.1.1.1', agreement.ip)
 
     def test_process_request_with_multiple_proxies(self):
         """Check IP of agreement with multiple proxies in place."""
-        self.client.post(self.agreement_url, data={'agree': 'on'}, HTTP_X_FORWARDED_FOR='1.1.1.1, 2.2.2.2, 3.3.3.3')
+        self.client.post(
+            self.agreement_url, data={'agree': 'on'},
+            HTTP_X_FORWARDED_FOR='1.1.1.1, 2.2.2.2, 3.3.3.3')
         agreement = SignedAgreement.objects.get(user=self.user)
         self.assertEqual('1.1.1.1', agreement.ip)
 
@@ -156,12 +178,16 @@ class TestExportCSV(TestCase):
     def test_export_csv(self):
         User.objects.create_user('lloyd', 'lloyd@example.com')
         User.objects.create_user('kratos', 'kratos@example.com')
+
         def generate_row(user):
             return (user.username, user.email)
 
         with patch('protected_assets.admin.timezone') as mock_timezone:
-            mock_timezone.now.return_value = aware_datetime(2014, 4, 1, 6, 5, 4)
-            response = export_csv(User.objects.order_by('id'), ('username', 'email'), generate_row)
+            mock_timezone.now.return_value = aware_datetime(
+                2014, 4, 1, 6, 5, 4)
+            response = export_csv(
+                User.objects.order_by('id'), ('username', 'email'),
+                generate_row)
 
         # We're kind've testing strftime below, but the bug required the
         # CSV to match what the admin interface shows, so we want to
