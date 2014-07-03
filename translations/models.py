c72fea9ad961cbe63fe78896c61be3a9c15625cf
Mark Lavin <mlavin@caktusgroup.com>
2014-07-03 15:42:16 -0400

    Starting work on copying CMS content when creating a new site.

--- translations/models.py
+++ translations/models.py
@@ -1,6 +1,11 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
 from django.db import models
+from django.db.models.query import QuerySet
+
+from concurrency.api import disable_concurrency
+
+from mezzanine.core.models import SiteRelated
 
 
 def get_site_for_language(language_code):
@@ -15,17 +20,59 @@ def get_site_for_language(language_code):
     return site
 
 
-def build_site_for_language(language_code):
+def build_site_for_language(language_code, copy_content=False):
     """Create a site for a given language."""
-    default = Site.objects.get_current()
     site = get_site_for_language(language_code)
     if site is None:
+        default = Site.objects.get_current()
         site = Site.objects.create(
             name=language_code,
             domain='%s.%s' % (language_code, default.domain)
         )
-        # TODO: Copy the current pages over
+        if copy_content:
+            build_site_content(site, base_site=default)
     else:
         # TODO: How to handle existing sites
         pass
     return site
+
+
+def _get_site_models():
+    """Get all model classes which subclass SiteRelated."""
+    return filter(lambda m: issubclass(m, SiteRelated), models.get_models())
+
+
+def build_site_content(site, base_site=None, models_to_copy=None):
+    """Build new site content by copying content from one site to another."""
+    if models_to_copy is None:
+        # Get all site related models
+        models_to_copy = _get_site_models()
+    base_site = base_site or Site.objects.get_current()
+    for model_cls in models_to_copy:
+        copies = []
+        # Use a QuerySet rather than the default manager because the
+        # CurrentSiteManager forces the site equal to the current SITE_ID
+        for instance in QuerySet(model_cls).filter(site=base_site):
+            instance.id = None
+            instance.site = site
+            copies.append(instance)
+        if copies:
+            if not model_cls._meta.parents:
+                QuerySet(model_cls).bulk_create(copies)
+            else:
+                # Multi-table inheritance classes can't be bulk created
+                new_ids = []
+                for c in copies:
+                    for _, field in model_cls._meta.parents.items():
+                        parent = getattr(c, field.name)
+                        parent.id = None
+                        setattr(c, field.name, parent)
+                    if hasattr(c, '_concurrencymeta'):
+                        with disable_concurrency(c):
+                            c.save(force_insert=True)
+                    else:
+                        c.save(force_insert=True)
+                    new_ids.append(c.id)
+                # SiteRelated.save will force this to the "current" site
+                # so these need to be updated with the site we actually want
+                QuerySet(model_cls).filter(id__in=new_ids).update(site=site)
--- translations/tests/test_models.py
+++ translations/tests/test_models.py
@@ -1,6 +1,9 @@
 from django.conf import settings
 from django.contrib.sites.models import Site
+from django.db.models.query import QuerySet
 from django.test import TestCase
+
+from mezzanine.pages.models import Page, RichTextPage, Link
 
 from .. import models
 
@@ -44,3 +47,42 @@ class BuildSiteForLanguageTestCase(TestCase):
         default = Site.objects.get_current()
         self.assertEqual(result, default)
         self.assertEqual(Site.objects.all().count(), 1)
+
+
+class BuildSiteContentTestCase(TestCase):
+    """Copy content from one site to another."""
+
+    def assertCopied(self, original, site):
+        """Assert that the model was copied to the new site."""
+        # Again use the QuerySet to get around the forced site filter
+        # when using the CurrentSiteManager
+        copied = QuerySet(original.__class__).filter(slug=original.slug, site=site)
+        print original.site_id
+        print QuerySet(original.__class__).all()
+        self.assertTrue(copied.exists())
+        self.assertEqual(copied.count(), 1)
+        self.assertTrue(copied.exists())
+        copy = copied[0]
+        print copy.site_id
+        self.assertNotEqual(copy.pk, original.pk)
+
+    def test_copy_default_minimal(self):
+        """Copy all CMS models from the default site (minimal example)."""
+        # This will be created with the default site
+        page = Page.objects.create(title='Build')
+        site = Site.objects.create(name='zh-cn', domain='example.com')
+        models.build_site_content(site)
+        self.assertCopied(page, site)
+
+    def test_copy_multiple_models(self):
+        """CMS Pages, RichTextPage, Links should all be copied."""
+        page = Page.objects.create(title='Build')
+        # TODO: .create fails because somehow related to django-concurrency
+        rich_page = RichTextPage(title='Learn', content='<h1>Title</h1>')
+        rich_page.save()
+        link = Link.objects.create(title='External Link')
+        site = Site.objects.create(name='zh-cn', domain='example.com')
+        models.build_site_content(site)
+        self.assertCopied(page, site)
+        self.assertCopied(rich_page, site)
+        self.assertCopied(link, site)
