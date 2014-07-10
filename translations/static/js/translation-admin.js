0e025f92939f54d0ff57a75a3086f2a8ae7bdd26
Mark Lavin <mlavin@caktusgroup.com>
2014-07-10 10:21:34 -0400

    Simulate the admin popup to show the original english version of the content.

--- translations/static/js/translation-admin.js
+++ translations/static/js/translation-admin.js
@@ -1,10 +1,27 @@
 (function ($, englishVersion) {
+    var popup = null;
+
+    function openPopup() {
+        if (popup === null || popup.closed) {
+            popup = window.open(
+                englishVersion.link,
+                englishVersion.label,
+                'height=800,width=800,resizable=yes,scrollbars=yes'
+            );
+        }
+        popup.focus();
+    }
+
     function buildToolsLink() {
         var li, link;
         if (englishVersion.link && englishVersion.label) {
             link = $('<a>')
                 .attr('href', englishVersion.link)
-                .text(englishVersion.label);
+                .text(englishVersion.label)
+                .click(function (e) {
+                    e.preventDefault();
+                    openPopup();
+                });
             li = $('<li>').append(link);
             $('ul.object-tools', '#content-main').prepend(li);
         }
@@ -13,4 +30,5 @@
     $(document).ready(function () {
         buildToolsLink();
     });
+
 })(jQuery, englishVersion);
