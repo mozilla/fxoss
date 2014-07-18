7ea23812cfbc00c9397e67e51a4a67f06b1fc8fe
Michael Kelly <mkelly@mozilla.com>
2014-07-18 13:47:31 -0400

    Fix 1032487: Add custom GA event tracking.

--- project/settings/base.py
+++ project/settings/base.py
@@ -313,6 +313,7 @@ MIDDLEWARE_CLASSES = [
     "mezzanine.pages.middleware.PageMiddleware",
     "mezzanine.core.middleware.FetchFromCacheMiddleware",
     "concurrency.middleware.ConcurrencyMiddleware",
+    "sandstone.middleware.SetJustLoggedInCookieMiddleware",
 ]
 
 # Store these package names here as they may change in the future since
