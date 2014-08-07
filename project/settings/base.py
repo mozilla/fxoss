ad596ddb7767c54cbc002c7b1fa66c88fa1da22b
David Ray <davidray@gmail.com>
2014-08-07 14:37:29 -0400

    i18n footer template bits

--- project/settings/base.py
+++ project/settings/base.py
@@ -297,6 +297,7 @@ TEMPLATE_CONTEXT_PROCESSORS = [
     "django.core.context_processors.request",
     "django.core.context_processors.tz",
     "mezzanine.conf.context_processors.settings",
+    "translations.context_processors.current_date"
 ]
 
 # List of middleware classes to use. Order is important; in the request phase,
